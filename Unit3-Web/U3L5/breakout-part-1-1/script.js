cb83818a08599d428536157d47711eaa61df50d5
liza-at-GWC <Liza@GWC-NYC-067.local>
2020-03-12 17:26:24 -0400

    added for specific parts of tutorial

--- Unit3-Web/U3L5/breakout-part-1-1/script.js
+++ Unit3-Web/U3L5/breakout-part-1-1/script.js
@@ -7,7 +7,7 @@ Sample code for Breakout Part 1
 by Girls Who Code
 March 2020
 
-NOTE: This reflects the code at the end of LESSON 5, PART 1
+NOTE: This reflects the code at the end of PART 1: CANVAS
 
 This project is adapted from the 2D breakout game using pure JavaScript
 https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript 
@@ -26,55 +26,10 @@ var ctx = canvas.getContext("2d");
 Draw a rectangle - This is just an example of how to draw a shape
 in Step 3. Move the * and / after the .closePath to comment it out or
 delete it.
+*/
 
 ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
 ctx.fillStyle = "#FF0000";
 ctx.fill();
 ctx.closePath();
-*/
-
-var x = canvas.width/8; //Starts at 60
-var y = canvas.height-30; //Starts at 290
-
-var dx = 2; //Value of the distance to move from x after each frame
-var dy = -2; //Value of the distance to move from y after each frame
-
-//The main loop
-function draw(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height); //Clears the canvas
-    ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
-    x += dx; //This is the same as x = x + dx
-    y += dy; //This is the same as y = y + dy
-    console.log("x = " + x + ", y = " + y);
-
-}
-//How often we clear and redraw the canvas
-setInterval(draw, 10);
-
-/*
-
-Here is the tidy code from step 6. It breaks the 
-draw() function into two parts.
-
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
-}
-
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-
-    x += dx;
-    y += dy;
-}
-
-*/
--- Unit3-Web/U3L5/breakout-part-1-3/script.js
+++ Unit3-Web/U3L5/breakout-part-1-3/script.js
@@ -7,7 +7,7 @@ Sample code for Breakout Part 1
 by Girls Who Code
 March 2020
 
-NOTE: This reflects the code at the end of LESSON 5, PART 3
+NOTE: This reflects the code at the end of PART 3: COLLISION
 
 This project is adapted from the 2D breakout game using pure JavaScript
 https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript 
@@ -30,15 +30,6 @@ var dy = -2; //Value of the distance to move from y after each frame
 
 var ballRadius = 10;
 
-var paddleHeight = 10;
-var paddleWidth = 75;
-var paddleX = (canvas.width-paddleWidth) / 2;
-
-//Booleans that store key presses for the paddle
-var rightPressed = false;
-var leftPressed = false;
-
-
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -47,18 +38,9 @@ function drawBall() {
     ctx.closePath();
 }
 
-function drawPaddle() {
-    ctx.beginPath();
-    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
-}
-
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-    drawPaddle();
 
     //Add collision detection for right and left of canvas
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
@@ -74,43 +56,8 @@ function draw() {
     y += dy; //This is the same as y = y + dy
 
     console.log("x = " + x + ", y = " + y);
-
-    if(rightPressed) {
-        paddleX += 7;
-        if (paddleX + paddleWidth > canvas.width){
-            paddleX = canvas.width - paddleWidth;
-        }
-    }
-    else if(leftPressed) {
-        paddleX -= 7;
-        if (paddleX < 0){
-            paddleX = 0;
-        }
-    }
-}
-
-//Event listeners for keys that control the paddle
-document.addEventListener("keydown", keyDownHandler);
-document.addEventListener("keyup", keyUpHandler);
-
-//Function for the keydown event listener
-function keyDownHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = true;
-    }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = true;
-    }
-}
-//Function for the keyup event listener
-function keyUpHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = false;
-    }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = false;
-    }
 }
 
 //How often we clear and redraw the canvas
 setInterval(draw, 10);
+
