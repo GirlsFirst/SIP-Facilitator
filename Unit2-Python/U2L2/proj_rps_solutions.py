66fab5080ea11b6109c07de5a2006a2a5a55c5b2
Samantha Ko <samanthako@Samanthas-MacBook-Pro.local>
2020-03-27 10:37:14 -0400

    updated starter

--- Unit2-Python/U2L2/proj_rps_solutions.py
+++ Unit2-Python/U2L2/proj_rps_solutions.py
@@ -63,3 +63,59 @@ if (play_wins==3):
     print ("Congratulations! You beat the computer")
 else:
     print("The computer beat you this time, but try again!")
+
+
+
+#Part 1: Introduction and Start of the Game
+#TODO: Write the opening introduction of your game
+print("Welcome to the automated Rock, Paper, Scissors Game!")
+print("The typical rules of the game is as follows:")
+print("You will choose either rock, paper, or scissors (remember that spelling counts!)")
+print("Rock beats Scissors")
+print("Paper beats Rock")
+print("Scissors beats Paper")
+print("You will be battling the computer in this game")
+print("In the event of a tie, no one wins the round")
+
+#TODO: Add variables to use throughout the program in this section
+choices = ["rock", "paper", "scissors"]
+round = 0
+player_win = 0
+comp_win = 0
+round_max = input("How many rounds should we play? ")
+
+
+
+
+#Part 2: Running Each Round
+#TODO: Update while loop to loop through as many rounds as you want for your game!
+while (round < round_max):            ):
+    #TODO: Ask for player's choice
+
+
+    #Randomly chooses the computer's choice from the choices list.
+    #The computer's choice will either be "rock", "paper", or "scissors"
+    comp = random.choice(choices)
+    print ("The computer chose" + comp)
+
+    #TODO: Add rules of game
+
+
+
+
+
+
+
+
+    #TODO: Increment the round variable
+    
+    
+    
+#Part 3: Finishing Results and End of the Game
+#TODO: Determine the overall winner
+
+
+
+
+
+#TODO: Add a statement here to let your player know the game has ended!
--- Unit2-Python/U2L2/proj_rps_starter.py
+++ Unit2-Python/U2L2/proj_rps_starter.py
@@ -8,19 +8,45 @@ Date Created:
 
 import random
 
-#Add variables to use throughout the program in this section
+#Part 1: Introduction and Start of the Game
+#TODO: Write the opening introduction of your game
+
+
+
+#TODO: Add variables to use throughout the program in this section
 choices = ["rock", "paper", "scissors"]
 
 
 
-#Update while loop to loop through as many rounds as you want for your game!
+#Part 2: Running Each Round
+#TODO: Update while loop to loop through as many rounds as you want for your game!
 while (              ):
-    
+    #TODO: Ask for player's choice
+
+
     #Randomly chooses the computer's choice from the choices list.
     #The computer's choice will either be "rock", "paper", or "scissors"
     comp = random.choice(choices)
-    
-    
-    
+    print ("The computer chose" + comp)
 
-#After all rounds have been completed add a statement here to let your player know the game has ended!
+    #TODO: Add rules of game
+
+
+
+
+
+
+
+
+    #TODO: Increment the round variable
+    
+    
+    
+#Part 3: Finishing Results and End of the Game
+#TODO: Determine the overall winner
+
+
+
+
+
+#TODO: Add a statement here to let your player know the game has ended!
