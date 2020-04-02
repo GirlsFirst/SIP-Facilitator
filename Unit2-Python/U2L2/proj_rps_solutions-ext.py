36f047a933bd09deee305f85810491d7d2a95ffd
samanthahe-gwc <sko62@gatech.edu>
2020-04-02 14:44:42 -0400

    updated solutions file with comments to locate ext

--- Unit2-Python/U2L2/proj_rps_solutions-ext.py
+++ Unit2-Python/U2L2/proj_rps_solutions-ext.py
@@ -12,7 +12,6 @@ Date Created: May 2020
 import random
 
 #Part 1: Introduction and Start of the Game
-#TODO: Write the opening introduction of your game
 print("Welcome to the automated Rock, Paper, Scissors Game!")
 print("The typical rules of the game is as follows:")
 print("You will choose either rock, paper, or scissors (remember that spelling counts!)")
@@ -22,11 +21,12 @@ print("Scissors beats Paper")
 print("You will be battling the computer in this game")
 print("In the event of a tie, no one wins the round")
 
-#TODO: Add variables to use throughout the program in this section
 choices = ["rock", "paper", "scissors"]
 round = 0
 player_win = 0
 comp_win = 0
+
+#Extensions #3-4: Allow user to determine number of rounds and check for invalid responses
 round_max = input("How many rounds should we play? ")
 if (round_max.isdigit() == True):
     round_max = int(round_max)
@@ -35,18 +35,15 @@ else:
 
 
 #Part 2: Running Each Round
-#TODO: Update while loop to loop through as many rounds as you want for your game!
 while (round < round_max):
-    #TODO: Ask for player's choice
     player = input("Pick rock, paper, or scissors: ")
+    
+    #Extensions #1: Accept responses regardless of case
     player = player.lower()
 
-    #Randomly chooses the computer's choice from the choices list.
-    #The computer's choice will either be "rock", "paper", or "scissors"
     comp = random.choice(choices)
     print ("The computer chose " + comp)
 
-    #TODO: Add rules of game
     if (player == "rock"):
         if (comp == "rock"):
             print("It's a tie")
@@ -77,12 +74,11 @@ while (round < round_max):
     else:
         print("I'm sorry, that is not a valid choice. Remember spelling counts!")
 
-    #TODO: Increment the round variable
     round+=1
+    #Extension #2: Adding current score at the end of each round
     print("SCORE: Player - " + str(player_win) + " Computer - " + str(comp_win))
     
 #Part 3: Finishing Results and End of the Game
-#TODO: Determine the overall winner
 if (comp_win > player_win):
     print("Sorry, you lost this time. The computer is the overall winner")
 elif (comp_win < player_win):
@@ -90,6 +86,5 @@ elif (comp_win < player_win):
 else:
     print("Both you and the computer tied!")
 
-#TODO: Add a statement here to let your player know the game has ended!
 print("The game has ended. Thank you for playing.")
 print("Rerun the program to play again!")
